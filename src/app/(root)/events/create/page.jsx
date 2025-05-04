ae9a9b8556ae48198765c5617a6c16ae680c8848
Srizon19 <50909722+Srizon19@users.noreply.github.com>
2025-05-04 23:02:21 +0600

    I am changing the stuffs

--- src/app/(root)/events/create/page.jsx
+++ src/app/(root)/events/create/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CreateEvent = () => {
     return (
         <div className="flex text-2xl p-20 justify-center font-bold bg-red-200">
-            CreateEvent
+            CreateEvent I did the changes MF
         </div>
     );
 };
