960b2a10661439f6363dcef995991d93e0b2a953
VoidAshfak <1703103@student.ruet.ac.bd>
2025-05-11 17:20:17 +0600

    persist store data in the browser local storage

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,10 +1,38 @@
+"use client";
+
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "@/lib/features/auth/authSlice"
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from "redux-persist"
+import storage from "redux-persist/lib/storage"
+
+const persistConfig = {
+    key: "root",
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, authSlice);
 
 export const makeStore = () => {
     return configureStore({
         reducer: {
-            auth: authSlice
+            auth: persistedReducer
         },
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
     })
 }
+
+export const store = makeStore();
+export const persistor = persistStore(store)
